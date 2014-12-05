2ff5b8e9105a0637db12730390ed7cad16d7a8f3
Thomas Röggla <tom@lichtfragmente.com>
2014-12-05 01:16:32 +0100

    solved thirteenth set of koans

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -3,21 +3,26 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (if (= (count coll) 1)
+    coll
+    (cons (last coll) (recursive-reverse (butlast coll)))))
 
 (defn factorial [n]
-  __)
+  (loop [fac 1 n n]
+    (if (zero? n)
+      fac
+      (recur (* fac n) (dec n)))))
 
 (meditations
   "Recursion ends with a base case"
